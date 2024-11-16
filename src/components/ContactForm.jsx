1cf0b4ee47b9c6481c05433c08d54c936b26d33b
Jakob Hjalmarsson <jakob.hjalmarsson@utb.ecutbildning.se>
2024-11-16 22:33:51 +0100

    Added simple form validation, added form validation with regular expressions and made the forms able to post the data to the api

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,6 +1,86 @@
+import { useRef } from "react";
+
 function ContactForm() {
+  const fullName = useRef(null);
+  const email = useRef(null);
+  const specialist = useRef(null);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    let isNameValid;
+    let isEmailValid;
+    let isSpecialistValid;
+
+    const regexName = /\d/;
+    const regexEmail = /^[^@\s]+@[^@\s]+\.[^@\s]+$/;
+
+    if (!fullName.current.value) {
+      fullName.current.classList.add("invalid");
+      isNameValid = false;
+    } else if (regexName.test(fullName.current.value)) {
+      fullName.current.classList.add("invalid");
+      isNameValid = false;
+    } else {
+      fullName.current.classList.remove("invalid");
+      isNameValid = true;
+    }
+    if (!email.current.value) {
+      email.current.classList.add("invalid");
+      isEmailValid = false;
+    } else if (!regexEmail.test(email.current.value)) {
+      email.current.classList.add("invalid");
+      isEmailValid = false;
+    } else {
+      email.current.classList.remove("invalid");
+      isEmailValid = true;
+    }
+    if (!specialist.current.value) {
+      specialist.current.classList.add("invalid");
+      isSpecialistValid = false;
+    } else {
+      specialist.current.classList.remove("invalid");
+      isSpecialistValid = true;
+    }
+
+    if (!isNameValid || !isEmailValid || !isSpecialistValid) {
+      alert("Form not valid!");
+    } else {
+      try {
+        const response = await fetch(
+          "https://win24-assignment.azurewebsites.net/api/forms/contact",
+          {
+            method: "POST",
+            body: JSON.stringify({
+              fullName: fullName.current.value,
+              email: email.current.value,
+              specialist: specialist.current.value,
+            }),
+            headers: {
+              accept: "application/json",
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
+        if (response.ok) {
+          // const data = await response.json();
+          // console.log(data);
+          fullName.current.value = "";
+          email.current.value = "";
+          specialist.current.value = "";
+          alert("Form sent successfully.");
+        } else {
+          alert("Form was sent but something went wrong!");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+
   return (
-    <form className="contact-form" noValidate>
+    <form onSubmit={handleSubmit} className="contact-form" noValidate>
       <h2 className="h3">Get Online Consultation</h2>
       <div className="input-fields">
         <div className="input-field">
@@ -8,6 +88,7 @@ function ContactForm() {
             Full name
           </label>
           <input
+            ref={fullName}
             type="text"
             name="name"
             id="name"
@@ -19,6 +100,7 @@ function ContactForm() {
             Email address
           </label>
           <input
+            ref={email}
             type="email"
             name="email"
             id="email"
@@ -30,6 +112,7 @@ function ContactForm() {
             Specialist
           </label>
           <select
+            ref={specialist}
             name="specialist"
             id="specialist"
             className="select"
--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,7 +1,59 @@
 import notification from "../assets/notification.svg";
 import mailLight from "../assets/mail-light.svg";
+import { useRef } from "react";
 
 function Newsletter() {
+  const email = useRef(null);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    let isEmailValid;
+    const regex = /^[^@\s]+@[^@\s]+\.[^@\s]+$/;
+
+    if (!email.current.value) {
+      email.current.classList.add("invalid");
+      isEmailValid = false;
+    } else if (!regex.test(email.current.value)) {
+      email.current.classList.add("invalid");
+      isEmailValid = false;
+    } else {
+      email.current.classList.remove("invalid");
+      isEmailValid = true;
+    }
+
+    if (!isEmailValid) {
+      alert("Form not valid!");
+    } else {
+      try {
+        const response = await fetch(
+          "https://win24-assignment.azurewebsites.net/api/forms/subscribe",
+          {
+            method: "POST",
+            body: JSON.stringify({
+              email: email.current.value,
+            }),
+            headers: {
+              accept: "application/json",
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
+        if (response.ok) {
+          // const data = await response.json();
+          // console.log(data);
+          email.current.value = "";
+          alert("You have successfully signed up to our newsletter.");
+        } else {
+          alert("Form was sent but something went wrong!");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+
   return (
     <div className="form-container">
       <div className="form-heading">
@@ -14,10 +66,11 @@ function Newsletter() {
           </span>
         </h2>
       </div>
-      <form className="form" noValidate>
+      <form onSubmit={handleSubmit} className="form" noValidate>
         <div className="inner-container">
           <img className="mail" src={mailLight} alt="mail" />
           <input
+            ref={email}
             size="100"
             type="email"
             name="email"
